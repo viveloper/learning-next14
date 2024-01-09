494cfacda787360af75e21a7ac2fbc2311e7a65c
viveloper <ucsik1234@gmail.com>
2024-01-10 00:03:09 +0900

    useTransition and regular state updates

--- src/app/(NoLayout)/react18-features/page.tsx
+++ src/app/(NoLayout)/react18-features/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import { SuggestionType } from '@/app/api/suggestion/route';
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 import useSWR from 'swr';
 
 export default function Page() {
   const [word, setWord] = useState('');
+  const [isPending, startTransition] = useTransition();
 
   const { data } = useSWR(
     `api/suggestion?word=${word}`,
@@ -17,19 +18,25 @@ export default function Page() {
     {
       revalidateOnFocus: false,
       shouldRetryOnError: false,
+      keepPreviousData: true,
     }
   );
 
   const isShowSuggestion = data && data.length > 0;
 
+  console.log(isPending);
+
   return (
     <div className="p-4">
+      <h3 className="text-xl">Word Suggestion</h3>
       <input
         className="border border-gray-400 p-2 outline-none"
         type="text"
         value={word}
         onChange={(e) => {
-          setWord(e.target.value);
+          startTransition(() => {
+            setWord(e.target.value);
+          });
         }}
       />
       {isShowSuggestion && (
